77c0928c9fc01d8fbe9054651ef9177498b78ed0
HundredPercentCoverage <bunglenutter@gmail.com>
2022-02-03 23:07:04 +0000

    First commit

---
+++ app/routes/jokes/index.tsx
@@ -0,0 +1,34 @@
+import { Joke } from "@prisma/client";
+import { Link, LoaderFunction, useLoaderData } from "remix"
+import { db } from "~/utils/db.server"
+
+type LoaderData = { randomJoke: Joke };
+
+export let loader: LoaderFunction = async () => {
+  const count = await db.joke.count();
+  const randomNumber = Math.floor(Math.random() * count);
+  const [randomJoke] = await db.joke.findMany({
+    take: 1,
+    skip: randomNumber
+  });
+
+  const data: LoaderData = { randomJoke };
+
+  return data;
+}
+
+export default function JokesIndexRoute() {
+  const data = useLoaderData<LoaderData>();
+
+  return (
+    <div>
+      <p>Here's a random joke:</p>
+      <p>
+        {data.randomJoke.content}
+      </p>
+      <Link to={data.randomJoke.id}>
+        "{data.randomJoke.name}" Permalink
+      </Link>
+    </div>
+  )
+}
